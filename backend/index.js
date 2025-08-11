def989f327faefaa356869f4ff30e87004871dd9
Harmandeep01 <sharmandeep@gmail.com>
2025-08-11 22:57:17 +0530

    Pulled on Local Machine

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,5 @@ app.use(express.json())
 
 app.use('/', testRoute)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
