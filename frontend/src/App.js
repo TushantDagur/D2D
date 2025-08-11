54eec6dd84f6c47e95734685678a4eb931c26683
Harmandeep01 <sharmandeep@gmail.com>
2025-08-11 23:02:37 +0530

    Port Changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3000/')
+    axios.get('http://localhost:5000/')
       .then(res => setMessage(res.data.message))
       .catch(err => console.error(err));
   }, []);
