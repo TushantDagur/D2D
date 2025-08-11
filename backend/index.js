34e201176c25c558ea3dc571d10e43d2f1138ad9
Harmandeep01 <sharmandeep@gmail.com>
2025-08-11 22:46:30 +0530

    Backend API test route added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const testRoute = require('./routes/testRoute');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'D2D backend server running!' });
-});
+app.use('/', testRoute)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
