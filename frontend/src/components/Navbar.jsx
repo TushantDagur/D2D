877d2f23e926efea8562579f4d526d9671f6ec01
TushantDagur <Tushantdagur125@gmail.com>
2025-08-15 22:33:38 +0530

    React-router-dom, make routes for Home, Login, Signup added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Phone, Menu, X } from "lucide-react";
 
 export default function Navbar() {
