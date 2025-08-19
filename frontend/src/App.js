b4ef9ecb0cb0f719e2552f6d427e9b77b4e0cfcd
TushantDagur <Tushantdagur125@gmail.com>
2025-08-20 01:12:51 +0530

    Dashboared added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   const [message, setMessage] = useState('');
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
